7fa7d215d6f391dafbc9d4c048a8bf077a999a7b
Adil <adilchhabra7@gmail.com>
2016-03-22 23:15:25 -0400

    Working Edit

--- Simple1.java
+++ Simple1.java
@@ -5,5 +5,12 @@ public class Simple1 extends DummySimple {
 	public void setUpFrame() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.getContentPane().setLayout(new FlowLayout());
+
+
+		JLabel label1 = new JLabel("Hello");
+		this.getContentPane().add(label1);
+
+		JLabel label2 = new JLabel("Bye");
+		this.getContentPane().add(label2);
 	}
 }
--- UseSimple.java
+++ UseSimple.java
@@ -1,3 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class UseSimple extends JFrame {
-	
+	public static void main(String [] args) {
+		DummySimple theFrame = new Simple1();
+		theFrame.setUpFrame();
+		theFrame.setPreferredSize(new Dimension(500,500));
+
+		theFrame.pack();
+		theFrame.setVisible(true);
+
+	}
 }
